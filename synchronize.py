3d94a57ffc7209e5cc73320932d9941b1cae5ad7
pedronogs <phnogs@hotmail.com>
2020-12-30 21:02:32 -0300

    [Feature] Patched synchronization method to be recursive (UPLOAD only, files inside folders)

--- synchronize.py
+++ synchronize.py
@@ -36,9 +36,9 @@ class Drive():
     
 
     # List all files inside specified Drive folder
-    def list_files(self):
+    def list_files(self, folder_id):
         # Call API
-        response = self.__service.files().list(q="'{}' in parents".format(CONFIGS['drive_folder_id'])).execute()
+        response = self.__service.files().list(q="'{}' in parents".format(folder_id)).execute()
 
         # Return all file names
         files_dic = {"all": response.get('files', []), "names": []}
@@ -73,42 +73,71 @@ class Drive():
 
 
     # Upload file from local to drive folder
-    def upload_file(self, filename):
+    def upload_file(self, filename, local_path, folder_id):
         # Custom file metadata for upload
-        file_metadata = {'name': filename, 'parents': [CONFIGS['drive_folder_id']]}
+        file_metadata = {'name': filename, 'parents': [folder_id]}
 
         # File definitions for upload
-        media = MediaFileUpload('{}/{}'.format(CONFIGS['local_folder_path'], filename))
+        media = MediaFileUpload('{}\\{}'.format(local_path, filename))
 
         # Send POST request for upload API
-        uploaded_files = self.__service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        try:
+            uploaded_file = self.__service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+            print('File uploaded successfully: {}'.format(filename))
 
-        print('File uploaded successfully: {}'.format(filename))
+            return uploaded_file
+        except:
+            return False
 
 
-    # Synchronize drive folder with local folder
-    def synchronize(self):
-        drive_files = self.list_files()
-        local_files = list_local_files()
+    # Upload file from local to drive folder
+    def upload_folder(self, foldername, folder_id):
+        # Custom folder metadata for upload
+        folder_metadata = {'name': foldername, 'parents': [folder_id], 'mimeType': 'application/vnd.google-apps.folder'}
+
+        try:
+            # Send POST request for upload API
+            uploaded_folder = self.__service.files().create(body=folder_metadata).execute()
+            print('Folder created with ID: {}'.format(uploaded_folder['id']))
+
+            return uploaded_folder['id']
+        except:
+            return False
+
+
+    # Recursive method to synchronize all folder and files
+    def synchronize(self, local_path, folder_id):
+        # List remote and local files
+        drive_files = self.list_files(folder_id)
+        local_files = list_local_files(local_path)
 
         # Find all different files between drive and local folders
         different_files = list(set(drive_files['names']) ^ set(local_files))
 
-        # Compare files in both folders and download/upload what is needed
+        # Compare files in both origins and download/upload what is needed
         for diff_file in different_files:
+            # IF file is only on Google Drive
             if diff_file in drive_files['names']:
-                # Find ID of file to be downloaded
                 for f in drive_files['all']:
                     if f['name'] == diff_file:
                         self.download_file(f['id'], f['name'])
+
+            # IF file is only on local
             else:
-                # Upload file
-                self.upload_file(diff_file)
-        
+                local_absolute_path = "{}\\{}".format(local_path, diff_file)
+
+                # Check if path redirects to a file or folder (and then upload)
+                if os.path.isdir(local_absolute_path):
+                    created_folder_id = self.upload_folder(diff_file, folder_id)
+                    if created_folder_id != False:
+                        self.synchronize(local_absolute_path, created_folder_id) # Recursive to upload files inside folders
+                else:
+                    self.upload_file(diff_file, local_path, folder_id)
+
 
 # Return list of all files in specified backup folder
-def list_local_files():
-    return os.listdir(CONFIGS['local_folder_path'])
+def list_local_files(local_path):
+    return os.listdir(local_path)
 
 
 def main():
@@ -122,7 +151,7 @@ def main():
     
     # Instantiate Drive class and synchronize files
     my_drive = Drive()
-    my_drive.synchronize()
+    my_drive.synchronize(CONFIGS['local_folder_path'], CONFIGS['drive_folder_id'])
 
 
 if __name__ == '__main__':
